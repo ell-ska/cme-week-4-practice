32b918fe09e32241753cae78d05be8e1aa263f73
ell-ska <ella@skagerstrom.se>
2024-10-25 14:26:22 +0200

    add secondary button

--- components/button.tsx
+++ components/button.tsx
@@ -10,6 +10,7 @@ const variants = cva(
     variants: {
       variant: {
         primary: 'hover:bg-primary-600 bg-primary text-white',
+        secondary: 'bg-zinc-800 text-white hover:bg-slate-700',
         tertiary: 'text-zinc-800 hover:bg-zinc-200',
       },
     },
